b71e9ae740fab96f39b84485a9aeef585df495e5
Darius Garcia <dariusgarcia888@gmail.com>
2023-10-30 14:32:32 -0700

    added navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Wholesale Haven</title>
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
